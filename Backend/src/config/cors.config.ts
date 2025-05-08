165ba284fb27755eef81fa66b35dc86a3451fc4c
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-05-08 15:36:18 +0530

    feat: login eye button

--- Backend/src/config/cors.config.ts
+++ Backend/src/config/cors.config.ts
@@ -2,8 +2,7 @@ import { CorsOptions } from "cors";
 import { env } from "./env.config";
 
 const corsOption: CorsOptions = {
-    // origin: env.CLIENT_URL,
-    origin: "https://draftly-blogs.vercel.app",
+    origin: env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
