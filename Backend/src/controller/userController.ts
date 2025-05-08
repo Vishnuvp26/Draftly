3b7c0739d16a308a238258faf80937762360f179
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-05-08 15:52:55 +0530

    fix: cors issue

--- Backend/src/controller/userController.ts
+++ Backend/src/controller/userController.ts
@@ -52,7 +52,7 @@ export const login = async (req: Request, res: Response, next: NextFunction): Pr
 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: true,
+            secure: process.env.NODE_ENV === "production",
             sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
