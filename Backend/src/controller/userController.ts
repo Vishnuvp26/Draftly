9464c5a1821912a0643486c5dd097aa7b5b3b980
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-05-08 16:27:16 +0530

    fix: refresh token bug

--- Backend/src/controller/userController.ts
+++ Backend/src/controller/userController.ts
@@ -52,7 +52,7 @@ export const login = async (req: Request, res: Response, next: NextFunction): Pr
 
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
             sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
