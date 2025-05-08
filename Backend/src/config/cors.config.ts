b592bc616d023732c7152c136bb21230dadbe036
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-05-08 15:29:02 +0530

    fix: cors

--- Backend/src/config/cors.config.ts
+++ Backend/src/config/cors.config.ts
@@ -2,7 +2,8 @@ import { CorsOptions } from "cors";
 import { env } from "./env.config";
 
 const corsOption: CorsOptions = {
-    origin: env.CLIENT_URL,
+    // origin: env.CLIENT_URL,
+    origin: "https://draftly-blogs.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
